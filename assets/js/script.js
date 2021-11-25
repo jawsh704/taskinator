0ca7cc84a8b99b1383d6f79dd83759d8a5a7fb91
YEET <joshuamani@hotmail.com>
2021-11-25 17:32:09 -0500

    made task type to display in task

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,22 @@ var taskToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold taskinfo and add to list item
+    var taskinfoEl = document.createElement("div");
+    taskinfoEl.className = "task-info";
+
+    // add html content to div
+    taskinfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskinfoEl)
+    
+    // add entire list item to list
     taskToDoEl.appendChild(listItemEl);
 };
 
